c3cbf4b4efc446011ef464f5b87fdf7974fcffd0
olell <olel@olel.de>
2025-07-23 18:09:55 +0200

    🚧 add phonebook page

--- app/web/routes/index.py
+++ app/web/routes/index.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse, RedirectResponse
 
+from app.core.db import SessionDep
 from app.web.deps import OptionalCurrentUser
 from app.web.templates import templates
+from app.models.crud.extension import filter_extensions_by_name
 
 router = APIRouter()
 
@@ -13,7 +15,16 @@ def index():
 
 
 @router.get("/phonebook", response_class=HTMLResponse)
-def phonebook(request: Request, user: OptionalCurrentUser):
+def phonebook(
+    *,
+    session: SessionDep,
+    request: Request,
+    query: str | None = None,
+    user: OptionalCurrentUser,
+):
+    phonebook_data = filter_extensions_by_name(session, user, query)
     return templates.TemplateResponse(
-        request=request, name="phonebook.j2.html", context={"user": user}
+        request=request,
+        name="phonebook.j2.html",
+        context={"user": user, "phonebook": phonebook_data, "query": query},
     )

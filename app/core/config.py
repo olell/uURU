olell/uURU

commit 3c793e1fd5e37859745b5b8cef754e8f190a66ef

✨ add mysql support

app/core/config.py
@@ -47,38 +47,39 @@ class Settings(BaseSettings):
 
     ## DATABASE
 
-    POSTGRES_SERVER: str
-    POSTGRES_PORT: int = 5432
-    POSTGRES_USER: str
-    POSTGRES_PASSWORD: str = ""
-    POSTGRES_DB: str = ""
-    POSTGRES_DB_TEST: str | None = None
+    DATABASE_TYPE: Literal["mysql"] | Literal["postgres"] = "postgres"
+    DATABASE_SERVER: str
+    DATABASE_PORT: int = 5432
+    DATABASE_USER: str
+    DATABASE_PASSWORD: str = ""
+    DATABASE_DB: str = ""
+    DATABASE_DB_TEST: str | None = None
 
     @computed_field  # type: ignore[prop-decorator]
     @property
     def SQLALCHEMY_TEST_DATABASE_URI(self) -> PostgresDsn:
-        if not self.POSTGRES_DB_TEST:
+        if not self.DATABASE_DB_TEST:
             return self.SQLALCHEMY_DATABASE_URI
 
         return MultiHostUrl.build(
-            scheme="postgresql+psycopg",
-            username=self.POSTGRES_USER,
-            password=self.POSTGRES_PASSWORD,
-            host=self.POSTGRES_SERVER,
-            port=self.POSTGRES_PORT,
-            path=self.POSTGRES_DB_TEST,
+            scheme="postgresql+psycopg" if self.DATABASE_TYPE == "postgres" else "mysql+pymysql",
+            username=self.DATABASE_USER,
+            password=self.DATABASE_PASSWORD,
+            host=self.DATABASE_SERVER,
+            port=self.DATABASE_PORT,
+            path=self.DATABASE_DB_TEST,
         )
 
     @computed_field  # type: ignore[prop-decorator]
     @property
     def SQLALCHEMY_DATABASE_URI(self) -> PostgresDsn:
         return MultiHostUrl.build(
-            scheme="postgresql+psycopg",
-            username=self.POSTGRES_USER,
-            password=self.POSTGRES_PASSWORD,
-            host=self.POSTGRES_SERVER,
-            port=self.POSTGRES_PORT,
-            path=self.POSTGRES_DB,
+            scheme="postgresql+psycopg" if self.DATABASE_TYPE == "postgres" else "mysql+pymysql",
+            username=self.DATABASE_USER,
+            password=self.DATABASE_PASSWORD,
+            host=self.DATABASE_SERVER,
+            port=self.DATABASE_PORT,
+            path=self.DATABASE_DB,
         )
 
     ## BEHAVIOR

docker-compose.yml
@@ -11,9 +11,10 @@ services:
     environment:
       UURU_SITE_NAME: "TARMAC"
       UURU_LIFESPAN_DROP_DB: 1
-      UURU_POSTGRES_SERVER: database
-      UURU_POSTGRES_USER: root
-      UURU_POSTGRES_PASSWORD: rootpasswd
-      UURU_POSTGRES_DB: uuru
+      UURU_DATABASE_TYPE: postgres
+      UURU_DATABASE_SERVER: database
+      UURU_DATABASE_USER: root
+      UURU_DATABASE_PASSWORD: rootpasswd
+      UURU_DATABASE_DB: uuru
     ports:
       - 8000:8000

pyproject.toml
@@ -13,6 +13,7 @@ dependencies = [
     "pydantic-extra-types>=2.10.5",
     "pydantic-settings>=2.10.1",
     "pyjwt>=2.10.1",
+    "pymysql>=1.1.1",
     "pytest>=8.4.1",
     "python-dotenv>=1.1.1",
     "sqlmodel>=0.0.24",

commit d5024aa58a1d11b5dd28444a54331f9618477ee8

🚑 fix mysql support by casting str to uuid
Fixes: 3c793e1fd5e37859745b5b8cef754e8f190a66ef

app/core/config.py
@@ -57,29 +57,41 @@ class Settings(BaseSettings):
 
     @computed_field  # type: ignore[prop-decorator]
     @property
-    def SQLALCHEMY_TEST_DATABASE_URI(self) -> PostgresDsn:
+    def SQLALCHEMY_TEST_DATABASE_URI(self) -> str:
         if not self.DATABASE_DB_TEST:
             return self.SQLALCHEMY_DATABASE_URI
 
-        return MultiHostUrl.build(
-            scheme="postgresql+psycopg" if self.DATABASE_TYPE == "postgres" else "mysql+pymysql",
-            username=self.DATABASE_USER,
-            password=self.DATABASE_PASSWORD,
-            host=self.DATABASE_SERVER,
-            port=self.DATABASE_PORT,
-            path=self.DATABASE_DB_TEST,
+        return str(
+            MultiHostUrl.build(
+                scheme=(
+                    "postgresql+psycopg"
+                    if self.DATABASE_TYPE == "postgres"
+                    else "mysql+pymysql"
+                ),
+                username=self.DATABASE_USER,
+                password=self.DATABASE_PASSWORD,
+                host=self.DATABASE_SERVER,
+                port=self.DATABASE_PORT,
+                path=self.DATABASE_DB_TEST,
+            )
         )
 
     @computed_field  # type: ignore[prop-decorator]
     @property
-    def SQLALCHEMY_DATABASE_URI(self) -> PostgresDsn:
-        return MultiHostUrl.build(
-            scheme="postgresql+psycopg" if self.DATABASE_TYPE == "postgres" else "mysql+pymysql",
-            username=self.DATABASE_USER,
-            password=self.DATABASE_PASSWORD,
-            host=self.DATABASE_SERVER,
-            port=self.DATABASE_PORT,
-            path=self.DATABASE_DB,
+    def SQLALCHEMY_DATABASE_URI(self) -> str:
+        return str(
+            MultiHostUrl.build(
+                scheme=(
+                    "postgresql+psycopg"
+                    if self.DATABASE_TYPE == "postgres"
+                    else "mysql+pymysql"
+                ),
+                username=self.DATABASE_USER,
+                password=self.DATABASE_PASSWORD,
+                host=self.DATABASE_SERVER,
+                port=self.DATABASE_PORT,
+                path=self.DATABASE_DB,
+            )
         )
 
     ## BEHAVIOR

app/models/crud/user.py
@@ -59,7 +59,7 @@ def delete_user(session: Session, executing_user: User, user_to_delete: User) ->
 
 
 def get_user_by_id(session: Session, user_id: uuid.UUID) -> User | None:
-    statement = select(User).where(User.id == user_id)
+    statement = select(User).where(User.id == uuid.UUID(user_id))
     user = session.exec(statement).first()
 
     return user
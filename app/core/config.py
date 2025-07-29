olell/uURU

commit 443026dd96e248acbdc506631c49686da1ce1359

✨ add reserved extensions

app/core/config.py
@@ -1,7 +1,11 @@
 import secrets
 from typing import Annotated, Any, Literal
 
-from pydantic import AnyUrl, BeforeValidator, PostgresDsn, computed_field
+from pydantic import (
+    AnyUrl,
+    BeforeValidator,
+    computed_field,
+)
 from pydantic_core import MultiHostUrl
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from pydantic_extra_types.color import Color
@@ -127,6 +131,8 @@ class Settings(BaseSettings):
 
     EXTENSION_TOKEN_PREFIX: str = "01990"
     EXTENSION_TOKEN_LENGTH: int = 8
+
+    RESERVED_EXTENSIONS: list[int | tuple[int, int]]
 
     ## SITE
 

app/api/extension.py
@@ -36,6 +36,10 @@ def create(
         raise HTTPException(
             status_code=status.HTTP_409_CONFLICT, detail="Extension already in use"
         )
+    except CRUDNotAllowedException as e:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN, detail=str(e)
+        )
 
 
 @router.patch("/{extension}", response_model=ExtensionBase)

app/models/crud/extension.py
@@ -11,11 +11,20 @@ from app.models.crud.asterisk import (
 )
 from app.models.user import User, UserRole
 from app.models.extension import ExtensionCreate, Extension, ExtensionUpdate
+from app.core.config import settings
 
 
 def create_extension(
     session: Session, session_asterisk: Session, user: User, extension: ExtensionCreate
 ) -> Extension:
+
+    if user.role != UserRole.ADMIN:  # check that the extension is not reserved
+        ext = int(extension.extension)
+        for rule in settings.RESERVED_EXTENSIONS:
+            if (isinstance(rule, int) and ext == rule) or (
+                isinstance(rule, tuple) and ext >= rule[0] and ext <= rule[1]
+            ):
+                raise CRUDNotAllowedException("This extension is reserved!")
 
     try:
         db_obj = Extension.model_validate(

app/web/routes/extension.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter, Request, status, Form, HTTPException
 from fastapi.responses import HTMLResponse, RedirectResponse
 
 from app.core.db import SessionAsteriskDep, SessionDep
+from app.models.crud import CRUDNotAllowedException
 from app.web.deps import CurrentUser
 from app.web.message import MessageBroker
 from app.web.templates import templates
@@ -49,7 +50,10 @@ def create_extension_handle(
     try:
         create_extension(session, session_asterisk, user, data)
     except Exception as e:
-        MessageBroker.push(request, {"message": "Failed to create extension", "category": "error"})
+        if isinstance(e, CRUDNotAllowedException):
+            MessageBroker.push(request, {"message": f"Failed to create extension: {str(e)}", "category": "error"})
+        else:
+            MessageBroker.push(request, {"message": "Failed to create extension", "category": "error"})
         return "/extension/create"
     MessageBroker.push(request, {"message": "Created extension!", "category": "success"})
     return "/extension/own"

templates/extension/create.j2.html
@@ -192,6 +192,8 @@
         />`: "");
   })
 
+  const reserved_extensions = JSON.parse('{{ settings.RESERVED_EXTENSIONS }}'.replace("(", "[").replace(")", "]"));
+
   $("#extensionForm").on("submit", (e) => {
     // sanity check mac input
     let ext_type = $("#typeInput").val();
@@ -206,12 +208,27 @@
       }
     }
 
+    // sanity check extension length
     let ext_length = $("#extensionInput").val().length
     if ((ext_length) != {{ settings.EXTENSION_DIGITS }}) {
         $("#formError").text("Extension must be {{ settings.EXTENSION_DIGITS }} digits long!");
         $("#formError").css("display", "block");
         e.preventDefault();
     }
+
+{% if user.role != 'admin' %}
+    // sanity check reserved extensions
+    reserved_extensions.forEach(rule => {
+      let ext = parseInt($("#extensionInput").val());
+      console.log(rule, ext);
+      if ((rule.length === 2 && ext >= rule[0] && ext <= rule[1]) || ext === rule) {
+        $("#formError").text("This extension is reserved, please use a different number!");
+        $("#formError").css("display", "block");
+        e.preventDefault();
+      }
+    });
+
+{% endif %}
   });
 
   

commit 7632f504e722ec9ba6c3d697e178bff5bcf86581

🐛 config: add default value for RESERVED_EXTENSIONS
Fixes: 443026dd96e248acbdc506631c49686da1ce1359

app/core/config.py
@@ -135,7 +135,7 @@ class Settings(BaseSettings):
     EXTENSION_TOKEN_PREFIX: str = "01990"
     EXTENSION_TOKEN_LENGTH: int = 8
 
-    RESERVED_EXTENSIONS: list[int | tuple[int, int]]
+    RESERVED_EXTENSIONS: list[int | tuple[int, int]] = []
     ALL_EXTENSION_TYPES_PUBLIC: bool = 0
 
     ## SITE
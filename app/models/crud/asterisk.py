olell/uURU

commit deb7cac3e2c9b23aed6735c36e8cbdb65a117475

✨ add SUPPORTED_CODEC

app/models/crud/asterisk.py
@@ -3,6 +3,7 @@ from sqlmodel import Session, delete, select
 from app.models.asterisk import PSAor, PSAuth, PSEndpoint
 from app.models.crud import CRUDNotAllowedException
 from app.models.extension import Extension
+from app.telephoning.main import Telephoning
 
 
 def create_asterisk_extension(
@@ -18,7 +19,14 @@ def create_asterisk_extension(
         )
         # TODO: fix hard coded context
         # TODO: fix hard coded transport
-        # TODO: fix hard coded codec
+
+        flavor = Telephoning.get_flavor_by_type(Extension.type)
+        codec = (
+            flavor.SUPPORTED_CODEC
+            if isinstance(flavor.SUPPORTED_CODEC, str)
+            else flavor.SUPPORTED_CODEC[Extension.type]
+        )
+
         ps_endpoint = PSEndpoint(
             id=extension.extension,
             transport="transport-udp",
@@ -26,7 +34,7 @@ def create_asterisk_extension(
             auth=ps_auth.id,
             context="pjsip_internal",
             disallow="all",
-            allow="g722",
+            allow=codec,
         )
         session_asterisk.add(ps_aor)
         session_asterisk.add(ps_auth)

app/telephoning/flavor.py
@@ -1,4 +1,5 @@
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Literal
+
 if TYPE_CHECKING:
     from app.models.extension import Extension
 
@@ -55,6 +56,14 @@ class PhoneFlavor:
     # Job interval in seconds
     JOB_INTERVAL: int = 60
 
+    # When extensions are created the asterisk phonetype is configured based
+    # on this value. If its a string, the value is applied for all phone types
+    # in this flavor class. If it is a dict, it has to configure values for
+    # all phone types from this class.
+    # Supported codecs:
+    CODEC = Literal["g722", "alaw", "ulaw", "g726", "gsm", "lpc10"]
+    SUPPORTED_CODEC: CODEC | dict[str, CODEC] = "g722"
+
     def on_extension_create(
         self, session: Session, asterisk_session: Session, extension: "Extension"
     ):
@@ -102,9 +111,9 @@ class PhoneFlavor:
     ## DO NOT OVERWRITE THOSE METHODS
     def is_public(self):
         return settings.ALL_EXTENSION_TYPES_PUBLIC or not self.IS_SPECIAL
-    
+
     def get_schema(self):
         if self.EXTRA_FIELDS is None:
             return None
-        
+
         return self.EXTRA_FIELDS.model_json_schema()

app/telephoning/phonetypes/innovaphone.py
@@ -17,7 +17,11 @@ class InnovaphoneFields(BaseModel):
 
 class Innovaphone(PhoneFlavor):
 
-    PHONE_TYPES = ["Innovaphone 241", "Innovaphone 201a"]
+    PHONE_TYPES = ["Innovaphone 241", "Innovaphone 200a"]
+    SUPPORTED_CODEC = {
+        "Innovaphone 241": "g722",
+        "Innovaphone 200a": "alaw"
+    }
     EXTRA_FIELDS = InnovaphoneFields
     IS_SPECIAL = True
 

commit 10b2eb33b0dc0e3060e7d9d8295d68fe66678a69

🐛 fix SUPPORTED_CODECS
Fixes: deb7cac3e2c9b23aed6735c36e8cbdb65a117475

app/models/crud/asterisk.py
@@ -17,14 +17,14 @@ def create_asterisk_extension(
             username=extension.extension,
             password=extension.password,
         )
+
         # TODO: fix hard coded context
         # TODO: fix hard coded transport
-
-        flavor = Telephoning.get_flavor_by_type(Extension.type)
+        flavor = Telephoning.get_flavor_by_type(extension.type)
         codec = (
             flavor.SUPPORTED_CODEC
             if isinstance(flavor.SUPPORTED_CODEC, str)
-            else flavor.SUPPORTED_CODEC[Extension.type]
+            else flavor.SUPPORTED_CODEC[extension.type]
         )
 
         ps_endpoint = PSEndpoint(

app/models/extension.py
@@ -70,7 +70,7 @@ class Extension(ExtensionBase, table=True):
     @property
     def get_flavor_model(self) -> BaseModel:
         flavor = Telephoning.get_flavor_by_type(self.type)
-        if flavor.EXTRA_FIELDS is None:
+        if flavor is None or flavor.EXTRA_FIELDS is None:
             return None
         
         return flavor.EXTRA_FIELDS.model_validate(self.extra_fields)
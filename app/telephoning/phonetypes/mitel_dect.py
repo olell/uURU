9fa9fe8982fc082b38f0deb36a96d9a15f6a9580
Gregor Michels <hirnpfirsich@brainpeach.de>
2025-08-02 23:50:21 +0200

    ✨ mitel_dect: added on_extension_delete and on_extension_update
    also puts OMM into Subscription mode
    and introduces locking mechanism

--- app/telephoning/phonetypes/mitel_dect.py
+++ app/telephoning/phonetypes/mitel_dect.py
@@ -3,6 +3,7 @@ from fastapi import status
 from fastapi.responses import PlainTextResponse
 import mitel_ommclient2
 from sqlmodel import Session
+from threading import Lock
 
 from app.core.security import generate_extension_password
 from app.models.crud.asterisk import (
@@ -34,6 +35,7 @@ class MitelDECT(PhoneFlavor):
     JOB_INTERVAL = 10
 
     def __init__(self):
+        self.lock = Lock()
         self.ommclient = mitel_ommclient2.OMMClient2(
             host=settings.OMM_HOST,
             port=settings.OMM_PORT,
@@ -86,6 +88,8 @@ class MitelDECT(PhoneFlavor):
             delete_tmp_extension(session, session_asterisk, tmp_extension)
 
     def job(self):
+        self.enable_subscription_mode()
+
         for device in self.get_unbound_devices():
             tmp_ext_id = TemporaryExtensions.generate_extension()
             password = generate_extension_password()
@@ -114,21 +118,52 @@ class MitelDECT(PhoneFlavor):
                         context="pjsip_dect_tmp",
                     )
 
+    def enable_subscription_mode(self):
+        with self.lock:
+            mode = self.ommclient.get_subscription_mode()
+            if mode != "Configured":
+                print("enabled dect subscription mode")
+                self.ommclient.set_subscription_mode("Configured")
+
     def get_unbound_devices(self):
-        return self.ommclient.find_devices(
-            lambda d: d.relType == mitel_ommclient2.types.PPRelTypeType("Unbound")
-        )
+        with self.lock:
+            return self.ommclient.find_devices(
+                lambda d: d.relType == mitel_ommclient2.types.PPRelTypeType("Unbound")
+            )
+
+    def get_user_by_extension(self, extension: str):
+        with self.lock:
+            users = list(self.ommclient.find_users(lambda u: u.num == extension))
+
+        if len(users) != 1:
+            raise RuntimeError(
+                f"found {len(users)} for extension {extension.extension} in OMM"
+            )
+        return users[0]
 
     def configure_user_on_device(
         self, ext_name: str, ext_id: int, ext_password: str, device_ppn: int
     ) -> int:
         user = self.ommclient.create_user(str(ext_id))
-        self.ommclient.set_user_sipauth(user.uid, str(ext_id), ext_password)
-        self.ommclient.set_user_name(user.uid, str(ext_name))
-        self.ommclient.attach_user_device(int(user.uid), device_ppn)
+        with self.lock:
+            self.ommclient.set_user_sipauth(user.uid, str(ext_id), ext_password)
+            self.ommclient.set_user_name(user.uid, str(ext_name))
+            self.ommclient.attach_user_device(int(user.uid), device_ppn)
 
         return int(user.uid)
 
     def delete_user_from_device(self, user_id: int, device_ppn: int) -> None:
-        self.ommclient.detach_user_device(uid=user_id, ppn=device_ppn)
-        self.ommclient.delete_pp_user(id=user_id)
+        with self.lock:
+            self.ommclient.detach_user_device(uid=user_id, ppn=device_ppn)
+            self.ommclient.delete_pp_user(id=user_id)
+
+    def on_extension_delete(self, session, asterisk_session, extension):
+        user = self.get_user_by_extension(extension.extension)
+        with self.lock:
+            self.ommclient.detach_user_device_by_user(user.uid)
+            self.ommclient.delete_pp_user(user.uid)
+
+    def on_extension_update(self, session, asterisk_session, extension):
+        user = self.get_user_by_extension(extension.extension)
+        with self.lock:
+            self.ommclient.set_user_name(user.uid, extension.name)

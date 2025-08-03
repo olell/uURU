olell/uURU

commit 63c6eade7e88388a710415d6c62625dcb51e4384

🥅 rolling back database transaction when one of multiple actions fail

app/models/crud/asterisk.py
@@ -51,14 +51,16 @@ def create_asterisk_extension(
         session_asterisk.add(ps_aor)
         session_asterisk.add(ps_auth)
         session_asterisk.add(ps_endpoint)
-        if autocommit:
-            session_asterisk.commit()
-            session_asterisk.refresh(ps_aor)
-            session_asterisk.refresh(ps_auth)
-            session_asterisk.refresh(ps_endpoint)
     except Exception as e:
+        session_asterisk.rollback()
         print(f"exception {e}")
         raise CRUDNotAllowedException(f"could not configure endpoint in asterisk: {e}")
+
+    if autocommit:
+        session_asterisk.commit()
+        session_asterisk.refresh(ps_aor)
+        session_asterisk.refresh(ps_auth)
+        session_asterisk.refresh(ps_endpoint)
 
     return [ps_aor, ps_auth, ps_endpoint]
 

app/models/crud/extension.py
@@ -57,20 +57,31 @@ def create_extension(
             },
         )
         session.add(db_obj)
-        if autocommit:
-            session.commit()
-            session.refresh(db_obj)
+
+        if not flavor.PREVENT_SIP_CREATION:
+            create_asterisk_extension(
+                session_asterisk,
+                extension=db_obj.extension,
+                password=db_obj.password,
+                type=db_obj.type,
+                autocommit=False,
+            )
+        flavor.on_extension_create(session, session_asterisk, db_obj)
+
     except sqlalchemy.exc.IntegrityError:
+        session.rollback()
+        session_asterisk.rollback()
         raise CRUDNotAllowedException("Extension not available")
 
-    if not flavor.PREVENT_SIP_CREATION:
-        create_asterisk_extension(
-            session_asterisk,
-            extension=db_obj.extension,
-            password=db_obj.password,
-            type=db_obj.type,
-        )
-    flavor.on_extension_create(session, session_asterisk, db_obj)
+    except Exception as e:
+        session.rollback()
+        session_asterisk.rollback()
+        raise e
+
+    if autocommit:
+        session.commit()
+        session.refresh(db_obj)
+        session_asterisk.commit()
 
     return db_obj
 
@@ -91,14 +102,20 @@ def update_extension(
     if flavor is None:
         raise CRUDNotAllowedException("Unkown phone type!")
 
-    data = update_data.model_dump(exclude_unset=True)
-    extension.sqlmodel_update(data)
-    session.add(extension)
+    try:
+        data = update_data.model_dump(exclude_unset=True)
+        extension.sqlmodel_update(data)
+        session.add(extension)
+
+        flavor.on_extension_update(session, session_asterisk, extension)
+    except Exception as e:
+        session.rollback()
+        session_asterisk.rollback()
+        raise e
+
     if autocommit:
         session.commit()
         session.refresh(extension)
-
-    flavor.on_extension_update(session, session_asterisk, extension)
 
     return extension
 
@@ -117,14 +134,21 @@ def delete_extension(
     if flavor is None:
         raise CRUDNotAllowedException("Unkown phone type!")
 
-    flavor.on_extension_delete(session, session_asterisk, extension)
-    if not flavor.PREVENT_SIP_CREATION:
-        delete_asterisk_extension(session_asterisk, extension)
+    try:
+        flavor.on_extension_delete(session, session_asterisk, extension)
+        if not flavor.PREVENT_SIP_CREATION:
+            delete_asterisk_extension(session_asterisk, extension, autocommit=False)
 
-    session.delete(extension)
+        session.delete(extension)
+    except Exception as e:
+        session.rollback()
+        session_asterisk.rollback()
+        raise e
+
     if autocommit:
         session.commit()
         session.refresh(user)
+        session_asterisk.commit()
 
 
 def delete_tmp_extension(
@@ -133,10 +157,17 @@ def delete_tmp_extension(
     tmp_extension: TemporaryExtensions,
     autocommit=True,
 ) -> None:
-    delete_asterisk_extension(session_asterisk, tmp_extension)
-    session.delete(tmp_extension)
+    try:
+        delete_asterisk_extension(session_asterisk, tmp_extension, autocommit=False)
+        session.delete(tmp_extension)
+    except Exception as e:
+        session.rollback()
+        session_asterisk.rollback()
+        raise e
+
     if autocommit:
         session.commit()
+        session_asterisk.commit()
 
 
 def get_extension_by_id(

commit fdd97932a636947047af0abb3dc6cea20799a16a

🐛 fixed pending rollback errors due to transactions after rollbacks
fixes: 63c6eade7e88388a710415d6c62625dcb51e4384

app/models/crud/asterisk.py
@@ -53,7 +53,8 @@ def create_asterisk_extension(
         session_asterisk.add(ps_auth)
         session_asterisk.add(ps_endpoint)
     except Exception as e:
-        session_asterisk.rollback()
+        if autocommit:
+            session_asterisk.rollback()
         print(f"exception {e}")
         raise CRUDNotAllowedException(f"could not configure endpoint in asterisk: {e}")
 
@@ -79,7 +80,8 @@ def update_asterisk_extension(
         ps_endpoint.callerid = f"{extension.name} <{extension.extension}>"
         session_asterisk.add(ps_endpoint)
     except Exception as e:
-        session_asterisk.rollback()
+        if autocommit:
+            session_asterisk.rollback()
         raise e
 
     if autocommit:
@@ -94,7 +96,8 @@ def delete_asterisk_extension(
             session_asterisk.exec(delete(cls).where(cls.id == extension.extension))
 
     except Exception as e:
-        session_asterisk.rollback()
+        if autocommit:
+            session_asterisk.rollback()
         raise e
 
     if autocommit:

app/models/crud/extension.py
@@ -71,13 +71,15 @@ def create_extension(
         flavor.on_extension_create(session, session_asterisk, db_obj)
 
     except sqlalchemy.exc.IntegrityError:
-        session.rollback()
-        session_asterisk.rollback()
+        if autocommit:
+            session.rollback()
+            session_asterisk.rollback()
         raise CRUDNotAllowedException("Extension not available")
 
     except Exception as e:
-        session.rollback()
-        session_asterisk.rollback()
+        if autocommit:
+            session.rollback()
+            session_asterisk.rollback()
         raise e
 
     if autocommit:
@@ -113,8 +115,9 @@ def update_extension(
 
         flavor.on_extension_update(session, session_asterisk, extension)
     except Exception as e:
-        session.rollback()
-        session_asterisk.rollback()
+        if autocommit:
+            session.rollback()
+            session_asterisk.rollback()
         raise e
 
     if autocommit:
@@ -146,8 +149,9 @@ def delete_extension(
 
         session.delete(extension)
     except Exception as e:
-        session.rollback()
-        session_asterisk.rollback()
+        if autocommit:
+            session.rollback()
+            session_asterisk.rollback()
         raise e
 
     if autocommit:
@@ -166,8 +170,9 @@ def delete_tmp_extension(
         delete_asterisk_extension(session_asterisk, tmp_extension, autocommit=False)
         session.delete(tmp_extension)
     except Exception as e:
-        session.rollback()
-        session_asterisk.rollback()
+        if autocommit:
+            session.rollback()
+            session_asterisk.rollback()
         raise e
 
     if autocommit:
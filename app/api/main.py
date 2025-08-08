8581591dc59bf2ddaf863c16b25a03931779c664
olell <olel@olel.de>
2025-08-08 11:12:08 +0200

    ✨ add types endpoint to api

--- app/api/main.py
+++ app/api/main.py
@@ -3,8 +3,10 @@ from fastapi import APIRouter
 from app.api import user
 from app.api import extension
 from app.api import site
+from app.api import telephoning
 
 router = APIRouter()
 router.include_router(user.router)
 router.include_router(extension.router)
 router.include_router(site.router)
+router.include_router(telephoning.router)

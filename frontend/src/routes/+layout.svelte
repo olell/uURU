8b615740dfb568f7e919e10b048440fb48a0ac97
olell <olel@olel.de>
2025-08-14 18:19:30 +0200

    🚧 changed to sveltestrap components

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,135 +1,130 @@
 <script lang="ts">
 	import favicon from '$lib/assets/favicon.svg';
-	import { colorMode } from '@sveltestrap/sveltestrap';
-	import { getSiteInfoApiV1SiteGet } from '../client';
-	import { site_info } from '../sharedState.svelte';
+	import {
+		Button,
+		Collapse,
+		colorMode,
+		Container,
+		Dropdown,
+		DropdownItem,
+		DropdownMenu,
+		DropdownToggle,
+		Nav,
+		Navbar,
+		NavbarBrand,
+		NavbarToggler,
+		NavItem,
+		NavLink
+	} from '@sveltestrap/sveltestrap';
+	import { getSiteInfoApiV1SiteGet, infoApiV1UserGet } from '../client';
+	import { site_info, user_info } from '../sharedState.svelte';
 
 	let { children } = $props();
-	let theme = $state()
+	let theme = $state();
+
+	let navbarOpen = $state(true);
+
+	function handleNavbarCollapse(event: any) {
+		navbarOpen = event.detail.isOpen;
+	}
 
 	// load site info
 	$effect(() => {
-		getSiteInfoApiV1SiteGet().then(({data, error}) => {
-			console.log(data, error)
+		getSiteInfoApiV1SiteGet()
+			.then(({ data, error }) => {
+				console.log(data, error);
+				if (error === undefined && data !== undefined) {
+					site_info.val = data;
+				}
+			})
+			.catch((e) => {
+				alert(e);
+			});
+	});
+
+	// load user info
+	$effect(() => {
+		infoApiV1UserGet().then(({ data, error }) => {
 			if (error === undefined && data !== undefined) {
-				site_info.val = data;
+				user_info.val = data;
 			}
-		}).catch((e) => {
-			alert(e);
-		})
-	})
+		});
+	});
 
 	// set theme based on system preference
 	$effect(() => {
-		const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)')
-		theme = (prefersDarkMode.matches ? 'dark' : 'light')
+		const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)');
+		theme = prefersDarkMode.matches ? 'dark' : 'light';
 
 		prefersDarkMode.addEventListener('change', (event) => {
-			theme = (event.matches ? 'dark' : 'light');
+			theme = event.matches ? 'dark' : 'light';
 		});
 	});
 	$effect(() => {
 		$colorMode = theme;
-	})
-
+	});
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-	<div class="container">
-		<a class="navbar-brand" href="/">
-			<img src={favicon} alt="" width="30" height="24" class="d-inline-block align-text-top" />
-			{site_info.val?.site_name} — µURU</a
-		>
-		<button
-			class="navbar-toggler"
-			type="button"
-			data-bs-toggle="collapse"
-			data-bs-target="#navbarSupportedContent"
-			aria-controls="navbarSupportedContent"
-			aria-expanded="false"
-			aria-label="Toggle navigation"
-		>
-			<span class="navbar-toggler-icon"></span>
-		</button>
-		<div class="collapse navbar-collapse" id="navbarSupportedContent">
-			<ul class="navbar-nav me-auto mb-2 mb-lg-0">
-				<li class="nav-item">
-					<a class="nav-link active" aria-current="page" href="/">Phonebook</a>
-				</li>
-				<li class="nav-item">
-					<a class="nav-link" aria-current="page" href="/map">Map</a>
-				</li>
-				<!-- {% if user is defined and user is not none %} -->
-				<li class="nav-item">
-					<a class="nav-link" href="/extension/own">Your Extensions</a>
-				</li>
-				<!-- {% endif %} -->
-				<!-- {% if pages.available() %} -->
-				<li class="nav-item dropdown">
-					<a
-						class="nav-link dropdown-toggle"
-						href="#"
-						role="button"
-						data-bs-toggle="dropdown"
-						aria-expanded="false"
-					>
-						PAGES
-					</a>
-					<ul class="dropdown-menu">
-						<!-- {% for page in pages.get_all() %} -->
-						<li><a class="dropdown-item" href="/pages/page">page</a></li>
-						<!-- {% endfor %} -->
-					</ul>
-				</li>
-				<!-- {% endif %} -->
-				<!-- {% if user is defined and user.role == "admin" %} -->
-				<li class="nav-item dropdown">
-					<a
-						class="nav-link dropdown-toggle"
-						href="#"
-						role="button"
-						data-bs-toggle="dropdown"
-						aria-expanded="false"
-					>
-						Admin
-					</a>
-					<ul class="dropdown-menu">
-						<li><a class="dropdown-item" href="/admin/users">Users</a></li>
-						<li><a class="dropdown-item" href="/admin/extensions">Extensions</a></li>
-					</ul>
-				</li>
-				<!-- {% endif %} -->
-			</ul>
-			<ul class="navbar-nav navbar-end">
-				<!-- {% if user is not defined or user is none %} -->
-				<li class="nav-item float-end">
-					<a class="nav-link" href="/user/login">Login</a>
-				</li>
-				<!-- {% else %}
-        <li class="nav-item dropdown">
-          <a
-            class="nav-link dropdown-toggle"
-            href="#"
-            role="button"
-            data-bs-toggle="dropdown"
-            aria-expanded="false"
-          >
-            {{ user.username }}
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="/user/settings">Settings</a></li>
-            <li><a class="dropdown-item" href="/user/logout">Logout</a></li>
-          </ul>
-        </li>
-        {% endif %} -->
-			</ul>
-		</div>
-	</div>
-</nav>
+<Navbar expand="md" container="md">
+	<NavbarBrand href="/">
+		<img src={favicon} alt="" width="30" height="24" class="d-inline-block align-text-top" />
+		{site_info.val?.site_name} — µURU
+	</NavbarBrand>
+	<NavbarToggler on:click={() => (navbarOpen = !navbarOpen)} />
+	<Collapse isOpen={navbarOpen} expand="md" navbar on:update={handleNavbarCollapse}>
+		<Nav navbar>
+			<NavItem>
+				<NavLink href="/">Phonebook</NavLink>
+			</NavItem>
+			<NavItem>
+				<NavLink href="/map">Map</NavLink>
+			</NavItem>
+			{#if user_info.val}
+				<NavItem>
+					<NavLink href="/extension/own">Your Extensions</NavLink>
+				</NavItem>
+			{/if}
+			<!-- {% if pages.available() %}
+			<Dropdown>
+				<DropdownToggle nav caret>Pages TODO: Title</DropdownToggle>
+				<DropdownMenu>
+					{#each ...}
+					<DropdownItem href="/pages?page=foo">page title</DropdownItem>
+					{/each}
+				</DropdownMenu>
+			</Dropdown>
+			{% endif %} -->
+			{#if user_info.val && user_info.val.role == 'admin'}
+			<Dropdown>
+				<DropdownToggle nav caret>Admin</DropdownToggle>
+				<DropdownMenu>
+					<DropdownItem href="/admin/user">Users</DropdownItem>
+					<DropdownItem href="/admin/extensions">Extensions</DropdownItem>
+				</DropdownMenu>
+			</Dropdown>
+			{/if}
+		</Nav>
+		<Nav class="ms-auto" navbar>
+			{#if !user_info.val}
+			<NavItem>
+				<NavLink href="/user/login">Login</NavLink>
+			</NavItem>
+			{:else}
+			<Dropdown>
+				<DropdownToggle nav caret>{user_info.val?.username}</DropdownToggle>
+				<DropdownMenu>
+					<DropdownItem href="/user/settings">Settings</DropdownItem>
+					<DropdownItem href="/user/logout">Logout</DropdownItem>
+				</DropdownMenu>
+			</Dropdown>
+			{/if}
+		</Nav>
+	</Collapse>
+</Navbar>
 
 <div class="container mt-3">
 	{@render children?.()}

3b02459fbf0e51143f97c9061d286c814512d812
olell <olel@olel.de>
2025-08-15 23:42:24 +0200

    🏗️ more verbose get phonetypes endpoint

--- app/api/telephoning.py
+++ app/api/telephoning.py
@@ -5,7 +5,9 @@ Copyright (c) Ole Lange, Gregor Michels and contributors. All rights reserved.
 Licensed under the MIT license. See LICENSE file in the project root for details.
 """
 
+from typing import Optional
 from fastapi import APIRouter
+from pydantic import BaseModel
 
 from app.api.deps import CurrentUser
 from app.models.user import UserRole
@@ -15,14 +17,22 @@ from app.telephoning.main import Telephoning
 router = APIRouter(prefix="/telephoning", tags=["telephoning"])
 
 
+class PhoneType(BaseModel):
+    schema: Optional[dict] = None
+    display_index: int
+    name: str
+
+
 @router.get("/types")
-def get_phone_types(user: CurrentUser):
-    schemas = {}
+def get_phone_types(user: CurrentUser) -> list[PhoneType]:
+    schemas: list[PhoneType] = []
     for flavor in Telephoning.instance().flavors.values():
         if flavor.is_public() or user.role == UserRole.ADMIN:
-            print(flavor)
-            schema = flavor.get_schema()
-            if schema is not None:
-                for phone_type in flavor.PHONE_TYPES:
-                    schemas.update({phone_type: schema})
+            for phone_type in flavor.PHONE_TYPES:
+                pt = PhoneType(
+                    schema=flavor.get_schema(),
+                    display_index=flavor.DISPLAY_INDEX,
+                    name=phone_type,
+                )
+                schemas.append(pt)
     return schemas

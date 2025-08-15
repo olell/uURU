bd6531bd7d432b6493e35868c9175cf2c6bc49ea
olell <olel@olel.de>
2025-08-15 16:44:30 +0200

    📱 made phonebook responsive

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,5 +1,13 @@
 <script lang="ts">
-	import { Form, FormGroup, Input } from '@sveltestrap/sveltestrap';
+	import {
+		Badge,
+		Form,
+		FormGroup,
+		Icon,
+		Input,
+		ListGroup,
+		ListGroupItem
+	} from '@sveltestrap/sveltestrap';
 	import {
 		adminPhonebookApiV1ExtensionAllGet,
 		phonebookApiV1ExtensionPhonebookGet,
@@ -8,6 +16,7 @@
 	} from '../client';
 	import { push_api_error, push_message } from '../messageService.svelte';
 	import { user_info } from '../sharedState.svelte';
+	import { onMount } from 'svelte';
 
 	let query = $state('');
 
@@ -42,6 +51,21 @@
 				});
 			});
 	});
+
+	let isMobile = $state(false);
+
+	function checkViewport() {
+		isMobile = window.innerWidth < 768; // Bootstrap md breakpoint
+	}
+
+	onMount(() => {
+		checkViewport();
+		window.addEventListener('resize', checkViewport);
+
+		return () => {
+			window.removeEventListener('resize', checkViewport);
+		};
+	});
 </script>
 
 <Form class="row g-3">
@@ -50,33 +74,63 @@
 	</FormGroup>
 </Form>
 
-<table class="table table-striped">
-	<thead>
-		<tr>
-			<th scope="col">#</th>
-			<th scope="col">Name</th>
-			<th scope="col">Location</th>
-			<th scope="col">Type</th>
-			{#if user_info.val?.role == 'admin'}
-				<th scope="col">Public</th>
-				<th scope="col">Created By</th>
-			{/if}
-		</tr>
-	</thead>
-	<tbody>
-		{#each filtered_phonebook as extension (extension.extension)}
+{#if !isMobile}
+	<table class="table table-striped">
+		<thead>
 			<tr>
-				<td>
-					<a href="tel:{extension.extension}">{extension.extension}</a>
-				</td>
-				<td>{extension.name}</td>
-				<td>{extension.location_name || ''}</td>
-				<td>{extension.type}</td>
+				<th scope="col">#</th>
+				<th scope="col">Name</th>
+				<th scope="col">Location</th>
+				<th scope="col">Type</th>
 				{#if user_info.val?.role == 'admin'}
-					<td>{extension.public ? '' : 'Not Public'}</td>
-					<td>{extension.created_by}</td>
+					<th scope="col">Public</th>
+					<th scope="col">Created By</th>
 				{/if}
 			</tr>
+		</thead>
+		<tbody>
+			{#each filtered_phonebook as extension (extension.extension)}
+				<tr>
+					<td>
+						<a href="tel:{extension.extension}">{extension.extension}</a>
+					</td>
+					<td>{extension.name}</td>
+					<td>{extension.location_name || ''}</td>
+					<td>{extension.type}</td>
+					{#if user_info.val?.role == 'admin'}
+						<td>{extension.public ? '' : 'Not Public'}</td>
+						<td>{extension.created_by}</td>
+					{/if}
+				</tr>
+			{/each}
+		</tbody>
+	</table>
+{:else}
+	<ListGroup>
+		{#each filtered_phonebook as extension (extension.extension)}
+			<ListGroupItem action href="tel:{extension.extension}">
+				<div class="d-flex w-100 justify-content-between">
+					<h5 class="mb-1">{extension.name}</h5>
+					<p class="mb-1 fw-bold font-monospace">
+						<Icon name="telephone-fill" />
+						{extension.extension}
+					</p>
+				</div>
+				<div class="d-flex w-100 justify-content-between mt-2">
+					{#if extension.location_name}
+						<Badge
+							pill
+							color="success"
+							class="d-inline-flex align-items-center justify-content-start"
+						>
+							<Icon name="geo-alt-fill" class="me-2" />
+							{extension.location_name}
+						</Badge>
+					{:else}<div></div>
+					{/if}
+					{extension.type}
+				</div>
+			</ListGroupItem>
 		{/each}
-	</tbody>
-</table>
+	</ListGroup>
+{/if}

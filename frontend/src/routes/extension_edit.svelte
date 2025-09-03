olell/uURU

commit 58bedc355fa26ea5072957adb30b5bbb802633e3

🦺 loading default value in SchemaForm

frontend/src/routes/extension_edit.svelte
@@ -110,8 +110,10 @@
 			return;
 		}
 		// create empty extra_data object to bind form elements to
-		Object.keys(selectedType.schema['properties'] as object).forEach((element) => {
-			extra_data[element] = extension?.extra_fields[element] || undefined;
+		let schema: any = selectedType.schema['properties'] as object;
+		Object.keys(schema).forEach((element) => {
+			extra_data[element] = extension?.extra_fields[element] || schema[element].default;
+			console.log('extra', $state.snapshot(extra_data)[element], schema[element]);
 		});
 	});
 

commit 2eadf61014ef8f684464a9726c5ebe22ac95b7de

🚑 remove logging the extra_data state
since the $state.snapshot causes the effect to fire on any changes to extra_data, those changes will immidiately be resetted which effectivly disables all extra_data inputs

fixes: 58bedc3

frontend/src/routes/extension_edit.svelte
@@ -113,7 +113,6 @@
 		let schema: any = selectedType.schema['properties'] as object;
 		Object.keys(schema).forEach((element) => {
 			extra_data[element] = extension?.extra_fields[element] || schema[element].default;
-			console.log('extra', $state.snapshot(extra_data)[element], schema[element]);
 		});
 	});
 
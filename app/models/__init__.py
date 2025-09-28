3c7f730fbc927fff25a964b2f849219a944278fb
olell <olel@olel.de>
2025-09-28 11:30:24 +0200

    🗃️ add iaxfriend database model

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -5,9 +5,9 @@ Copyright (c) Ole Lange, Gregor Michels and contributors. All rights reserved.
 Licensed under the MIT license. See LICENSE file in the project root for details.
 """
 
-from app.models.asterisk import PSAor, PSAuth, PSEndpoint
+from app.models.asterisk import PSAor, PSAuth, PSEndpoint, IAXFriend
 from app.models.user import Invite, User
 from app.models.extension import Extension, TemporaryExtensions
 
 tables = [User, Extension, TemporaryExtensions, Invite]
-asterisk_tables = [PSAor, PSAuth, PSEndpoint]
+asterisk_tables = [PSAor, PSAuth, PSEndpoint, IAXFriend]

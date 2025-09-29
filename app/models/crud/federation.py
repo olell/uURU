olell/uURU

commit 0e93b0ae19a279241e72227b48cfc4bb8fb40d9f

🩹 disabled cascading autocommit to rollback in case of error

app/models/crud/federation.py
@@ -213,7 +213,7 @@ def accept_incoming_peering_request(
     )
 
     try:
-        create_asterisk_iax_peer_and_dialplan(session_asterisk, peer, autocommit)
+        create_asterisk_iax_peer_and_dialplan(session_asterisk, peer, False)
 
         session.delete(request)
         session.add(peer)
@@ -279,7 +279,7 @@ def teardown_peer(
         )
 
     try:
-        delete_asterisk_iax_peer_and_dialplan(session_asterisk, peer, autocommit)
+        delete_asterisk_iax_peer_and_dialplan(session_asterisk, peer, False)
 
         session.delete(Peer)
         if autocommit:
@@ -389,7 +389,7 @@ def accept_outgoing_peering_request(
     try:
         session.delete(request)
         session.add(peer)
-        create_asterisk_iax_peer_and_dialplan(session_asterisk, peer, autocommit)
+        create_asterisk_iax_peer_and_dialplan(session_asterisk, peer, False)
 
         if autocommit:
             session.commit()
@@ -430,7 +430,7 @@ def request_peer_teardown(
         raise CRUDNotAllowedException("Unknown peer!")
 
     try:
-        delete_asterisk_iax_peer_and_dialplan(session_asterisk, peer, autocommit)
+        delete_asterisk_iax_peer_and_dialplan(session_asterisk, peer, False)
         session.delete(peer)
         if autocommit:
             session.commit()

commit 138b9f5ffcc5ccdd457e88810eec26923c575fec

🚑 fixed missing commit/rollback of asterisk session
fixes: 0e93b0ae19a279241e72227b48cfc4bb8fb40d9f

app/models/crud/federation.py
@@ -220,12 +220,14 @@ def accept_incoming_peering_request(
         session.delete(request)
         if autocommit:
             session.commit()
+            session_asterisk.commit()
 
         logger.info(f"Accepted peering {peer.name} with {peer.partner_uuru_host}")
 
     except:
         if autocommit:
             session.rollback()
+            session_asterisk.rollback()
         raise
 
 
@@ -284,11 +286,13 @@ def teardown_peer(
         session.delete(Peer)
         if autocommit:
             session.commit()
+            session_asterisk.commit()
 
         logger.info(f"Teared down peering {peer.name} with {peer.partner_uuru_host}")
     except:
         if autocommit:
             session.rollback()
+            session_asterisk.rollback()
         raise
 
 
@@ -393,9 +397,11 @@ def accept_outgoing_peering_request(
 
         if autocommit:
             session.commit()
+            session_asterisk.commit()
     except:
         if autocommit:
             session.rollback()
+            session_asterisk.rollback()
         raise
 
 
@@ -434,9 +440,11 @@ def request_peer_teardown(
         session.delete(peer)
         if autocommit:
             session.commit()
+            session_asterisk.commit()
     except:
         if autocommit:
             session.rollback()
+            session_asterisk.rollback()
         raise
 
 
44377dc6d37306a1da22d395670729c9835e5be3
olell <olel@olel.de>
2025-10-01 15:26:24 +0200

    ✨ add format models

--- app/models/media.py
+++ app/models/media.py
@@ -7,9 +7,10 @@ Licensed under the MIT license. See LICENSE file in the project root for details
 
 from datetime import datetime
 from enum import Enum
-from typing import Optional
+from typing import Literal, Optional
 import uuid
-from sqlmodel import Field, Relationship, SQLModel
+from pydantic import BaseModel, computed_field
+from sqlmodel import JSON, Column, Field, Relationship, SQLModel
 
 from app.models.user import User
 
@@ -20,6 +21,22 @@ class MediaType(str, Enum):
     RAW = "raw"
 
 
+class ImageFormat(BaseModel):
+    # please note: the out_type literal format must match util.media.SUPPORTED_IMAGE_FORMATS
+    out_type: Literal["avif", "bmp", "gif", "jpeg", "png", "tiff", "webp"] = "png"
+    colormode: Literal["1", "L", "RGB"] = "RGB"
+    width: Optional[int] = None
+    height: Optional[int] = None
+
+
+class AudioFormat(BaseModel):
+    # please note: the out_type literal format must match util.media.SUPPORTED_AUDIO_FORMATS
+    out_type: Literal["gsm", "wav", "ogg", "mp3", "flac"] = "mp3"
+    samplerate: Optional[int] = 44100
+    channels: Optional[int] = 2
+    bitdepth: Optional[int] = 16
+
+
 class Media(SQLModel, table=True):
     id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
 
@@ -27,6 +44,18 @@ class Media(SQLModel, table=True):
     type: MediaType
 
     created_by_id: Optional[uuid.UUID] = Field(default=None, foreign_key="user.id")
-    created_by: Optional[User] = Relationship(back_populates="assets")
+    created_by: Optional[User] = Relationship(back_populates="media")
 
     uploaded_at: datetime = Field(default_factory=datetime.now)
+
+    default_format_json: dict = Field(default_factory=dict, sa_column=Column(JSON))
+
+    @computed_field
+    @property
+    def default_format(self) -> AudioFormat | ImageFormat:
+        if self.type == MediaType.IMAGE:
+            return ImageFormat.model_validate(self.default_format_json)
+        elif self.type == MediaType.AUDIO:
+            return AudioFormat.model_validate(self.default_format_json)
+        else:
+            raise ValueError("Raw media does not have a default format!")

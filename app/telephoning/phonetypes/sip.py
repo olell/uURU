767565cdac13b3868ad8fae90882275ac8f03ca6
olell <olel@olel.de>
2025-10-09 14:58:13 +0200

    ✨ create music on hold database entry based on assigned media

--- app/telephoning/phonetypes/sip.py
+++ app/telephoning/phonetypes/sip.py
@@ -6,12 +6,21 @@ Licensed under the MIT license. See LICENSE file in the project root for details
 """
 
 from pydantic import BaseModel
+
+from app.core.config import settings
+from app.models.asterisk import MusicOnHold
 from app.models.crud.asterisk import (
+    create_music_on_hold,
     create_sip_account,
+    delete_music_on_hold,
     delete_sip_account,
+    get_music_on_hold_by_name,
+    update_music_on_hold,
     update_sip_account,
 )
-from app.telephoning.flavor import CODEC, PhoneFlavor
+from app.models.crud.dialplan import Dial, Dialplan
+from app.models.media import AudioFormat, MediaType
+from app.telephoning.flavor import CODEC, MediaDescriptor, PhoneFlavor
 
 
 class SIPFields(BaseModel):
@@ -24,6 +33,17 @@ class SIP(PhoneFlavor):
     DISPLAY_INDEX = 1000
     EXTRA_FIELDS = SIPFields
 
+    MEDIA = {
+        "moh": MediaDescriptor(
+            media_type=MediaType.AUDIO,
+            required=False,
+            label="Ringback Tone / Music on Hold",
+            out_format=AudioFormat(
+                out_type="mp3", samplerate=44100, channels=1, bitdepth=16
+            ),
+        )
+    }
+
     def on_extension_create(self, session, asterisk_session, user, extension):
         super().on_extension_create(session, asterisk_session, user, extension)
         create_sip_account(
@@ -34,6 +54,7 @@ class SIP(PhoneFlavor):
             codec=self.get_codec(extension),
             autocommit=False,
         )
+        create_music_on_hold(asterisk_session, extension, autocommit=False)
 
     def on_extension_update(self, session, asterisk_session, user, extension):
         super().on_extension_update(session, asterisk_session, user, extension)
@@ -42,6 +63,7 @@ class SIP(PhoneFlavor):
             extension,
             autocommit=False,
         )
+        update_music_on_hold(asterisk_session, extension, autocommit=False)
 
     def on_extension_delete(self, session, asterisk_session, user, extension):
         super().on_extension_delete(session, asterisk_session, user, extension)
@@ -50,6 +72,7 @@ class SIP(PhoneFlavor):
             extension.extension,
             autocommit=False,
         )
+        delete_music_on_hold(asterisk_session, extension, autocommit=False)
 
     def get_codec(self, extension) -> CODEC:
         """

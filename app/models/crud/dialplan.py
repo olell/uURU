caff142ca9683800043eb1db387aeae6c69fd174
olell <olel@olel.de>
2025-10-11 18:26:59 +0200

    ✨ implemented some more dialplan applications

--- app/models/crud/dialplan.py
+++ app/models/crud/dialplan.py
@@ -105,6 +105,102 @@ class Dial(BaseDialplanApp):
         return self.COMPATIBLE_APP, appdata
 
 
+class Answer(BaseDialplanApp):
+    # TODO: Answer has options https://docs.asterisk.org/Latest_API/API_Documentation/Dialplan_Applications/Answer/#synopsis
+    COMPATIBLE_APP: ClassVar = "Answer"
+
+    @staticmethod
+    def parse(_, appdata):
+        return Answer()
+
+    def assemble(self):
+        return self.COMPATIBLE_APP, ""
+
+
+class Hangup(BaseDialplanApp):
+    COMPATIBLE_APP: ClassVar = "Dial"
+    causecode: Optional[str] = None
+
+    @staticmethod
+    def parse(_, appdata):
+        if appdata is not None and len(appdata) > 0:
+            return Hangup(causecode=appdata)
+        return Hangup()
+
+    def assemble(self):
+        causecode = "" if self.causecode is None else self.causecode
+        return self.COMPATIBLE_APP, causecode
+
+
+class Set(BaseDialplanApp):
+    COMPATIBLE_APP: ClassVar = "Set"
+
+    name: str
+    value: str
+    inherit_mode: Literal["none", "inherit", "inherit_children"] = "none"
+
+    @staticmethod
+    def parse(_, appdata: str):
+        name, value = appdata.split("=")
+        inherit_mode = "none"
+        if name.startswith("__"):
+            name = name[2:]
+            inherit_mode = "inherit_children"
+        elif name.startswith("_"):
+            name = name[1:]
+            inherit_mode = "inherit"
+        return Set(name=name, value=value, inherit_mode=inherit_mode)
+
+    def assemble(self):
+        prefix = (
+            "_"
+            if self.inherit_mode == "inherit"
+            else "__" if self.inherit_mode == "inherit_children" else ""
+        )
+        return self.COMPATIBLE_APP, f"{prefix}{self.name}={self.value}"
+
+
+class Goto(BaseDialplanApp):
+    COMPATIBLE_APP: ClassVar = "Goto"
+
+    context: Optional[str] = None
+    extension: Optional[str] = None
+    priority: Optional[str] = None
+
+    @staticmethod
+    def parse(_, appdata):
+        args = appdata.split(",")
+        if len(args) == 3:
+            return Goto(context=args[0], extension=args[1], priority=args[2])
+        elif len(args) == 2:
+            return Goto(extension=args[0], priority=args[1])
+        else:
+            return Goto(priority=args[0])
+
+    def assemble(self):
+        appdata = ""
+        if self.context is not None and self.extension is not None:
+            appdata += f"{self.context},{self.extension},"
+        elif self.extension is not None:
+            appdata += f"{self.extension},"
+        appdata += self.priority
+        return self.COMPATIBLE_APP, appdata
+
+
+class ConfBridge(BaseDialplanApp):
+    COMPATIBLE_APP: ClassVar = "ConfBridge"
+
+    conference: str
+    # TODO: There are more options: https://docs.asterisk.org/Latest_API/API_Documentation/Dialplan_Applications/ConfBridge/#description
+
+    @staticmethod
+    def parse(appdata):
+        return ConfBridge(conference=appdata)
+
+    def assemble(self):
+        return self.COMPATIBLE_APP, self.conference
+
+
 class Dialplan:
 
     def __init__(self, session_asterisk: Session, exten: str, context="pjsip_internal"):

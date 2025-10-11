73065eb9d6824e61e5fb2c476f37a23fd389a655
olell <olel@olel.de>
2025-10-11 13:15:05 +0200

    ✨ implemented endpoints and crud method to retrieve contacts

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -5,7 +5,14 @@ Copyright (c) Ole Lange, Gregor Michels and contributors. All rights reserved.
 Licensed under the MIT license. See LICENSE file in the project root for details.
 """
 
-from app.models.asterisk import MusicOnHold, PSAor, PSAuth, PSEndpoint, IAXFriend
+from app.models.asterisk import (
+    MusicOnHold,
+    PSAor,
+    PSAuth,
+    PSEndpoint,
+    IAXFriend,
+    PSContact,
+)
 from app.models.user import Invite, User
 from app.models.extension import Extension, TemporaryExtensions
 from app.models.federation import Peer, IncomingPeeringRequest, OutgoingPeeringRequest
@@ -26,6 +33,7 @@ asterisk_tables = [
     PSAor,
     PSAuth,
     PSEndpoint,
+    PSContact,
     IAXFriend,
     MusicOnHold,
 ]
